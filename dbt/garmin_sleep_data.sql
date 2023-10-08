2d99932757bfea7757ac6268869d6a7af456b034
Nathan Glover <nathan@glovers.id.au>
2023-10-08 12:22:24 +0000

    added garmin_sleep_data schema

---
+++ dbt/garmin_sleep_data.sql
@@ -0,0 +1,115 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS garmin_sleep (
+    bodyBatteryChange int,
+    restingHeartRate int,
+    remSleepData boolean,
+    restlessMomentsCount int,
+    dailySleepDTO struct<
+        id: bigint,
+        userProfilePK: int,
+        calendarDate: date,
+        sleepTimeSeconds: int,
+        napTimeSeconds: int,
+        sleepWindowConfirmed: boolean,
+        sleepWindowConfirmationType: string,
+        sleepStartTimestampGMT: bigint,
+        sleepEndTimestampGMT: bigint,
+        sleepStartTimestampLocal: bigint,
+        sleepEndTimestampLocal: bigint,
+        autoSleepStartTimestampGMT: bigint,
+        autoSleepEndTimestampGMT: bigint,
+        sleepQualityTypePK: bigint,
+        sleepResultTypePK: bigint,
+        unmeasurableSleepSeconds: int,
+        deepSleepSeconds: int,
+        lightSleepSeconds: int,
+        remSleepSeconds: int,
+        awakeSleepSeconds: int,
+        deviceRemCapable: boolean,
+        retro: boolean,
+        sleepFromDevice: boolean,
+        averageSpO2Value: float,
+        lowestSpO2Value: int,
+        highestSpO2Value: int,
+        averageSpO2HRSleep: float,
+        averageRespirationValue: float,
+        lowestRespirationValue: float,
+        highestRespirationValue: float,
+        awakeCount: int,
+        avgSleepStress: float,
+        ageGroup: string,
+        sleepScoreFeedback: string,
+        sleepScoreInsight: string,
+        sleepVersion: int
+    >,
+    sleepMovement array<struct<
+        startGMT: timestamp,
+        endGMT: timestamp,
+        activityLevel: float
+    >>,
+    sleepLevels array<struct<
+        startGMT: string,
+        endGMT: string,
+        activityLevel: float
+    >>,
+    sleepRestlessMoments array<struct<
+        value: int,
+        startGMT: bigint
+    >>,
+    wellnessSpO2SleepSummaryDTO struct<
+        userProfilePk: int,
+        deviceId: bigint,
+        sleepMeasurementStartGMT: string,
+        sleepMeasurementEndGMT: string,
+        alertThresholdValue: int,
+        numberOfEventsBelowThreshold: int,
+        durationOfEventsBelowThreshold: int,
+        averageSPO2: float,
+        averageSpO2HR: float,
+        lowestSPO2: int
+    >,
+    wellnessEpochSPO2DataDTOList array<struct<
+        userProfilePK: int,
+        epochTimestamp: timestamp,
+        deviceId: bigint,
+        calendarDate: timestamp,
+        epochDuration: int,
+        spo2Reading: int,
+        readingConfidence: int
+    >>,
+    wellnessEpochRespirationDataDTOList array<struct<
+        startTimeGMT: bigint,
+        respirationValue: float
+    >>,
+    sleepHeartRate array<struct<
+        value: int,
+        startGMT: bigint
+    >>,
+    sleepStress array<struct<
+        value: int,
+        startGMT: bigint
+    >>,
+    sleepBodyBattery array<struct<
+        value: int,
+        startGMT: bigint
+    >>
+)
+PARTITIONED BY (
+    year string,
+    month string,
+    day string
+)
+ROW FORMAT SERDE 'org.apache.hive.hcatalog.data.JsonSerDe'
+WITH SERDEPROPERTIES ( "timestamp.formats"="yyyy-MM-dd'T'HH:mm:ss.S" )
+LOCATION 's3://garmin-connection-aws-cron-s3bucket-ucg6p4b0lajd/sleep/'
+TBLPROPERTIES (
+    "projection.enabled" = "true",
+    "projection.year.type" = "integer",
+    "projection.year.range" = "2023,2033",
+    "projection.month.type" = "integer",
+    "projection.month.range" = "1,12",
+    "projection.month.digits" = "2",
+    "projection.day.type" = "integer",
+    "projection.day.range" = "1,31",
+    "projection.day.digits" = "2",
+    "storage.location.template" = "s3://garmin-connection-aws-cron-s3bucket-ucg6p4b0lajd/sleep/${year}-${month}-${day}"
+);
