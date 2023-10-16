e18e78b73d900ceec13c6abb9fb75f960c45ca43
Kallan Gerard <36556982+kallangerard@users.noreply.github.com>
2023-10-16 00:16:28 +0000

    updating variable names to handle clients outside handler scope

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ import os
 import uuid
 
 import boto3
-from garth.http import Client
+from garth.http import Client as GarthClient
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -15,10 +15,10 @@ garmin_s3_bucket = os.environ['GARMIN_S3_BUCKET']
 garmin_user = get_secret_value(os.environ['GARMIN_USERNAME'])
 garmin_pass = get_secret_value(os.environ['GARMIN_PASSWORD'])
 
-garth_client = Client()
+garth_client = GarthClient()
 garth_client.login(garmin_user, garmin_pass)
 
-client = boto3.client('s3')
+s3_client = boto3.client('s3')
 
 
 def get_secret_value(secret_arn: str):
@@ -36,38 +36,38 @@ def get_secret_value(secret_arn: str):
     return response.get('SecretString')
 
 
-def sleep_data(garth_client: Client) -> dict:
+def sleep_data(client: GarthClient) -> dict:
     """
     Retrieves the daily sleep data for the user associated with the provided Garmin client.
 
     Args:
-        garth_client (Client): The Garmin Connect API client.
+        client (GarthClient): The Garmin Connect API client.
 
     Returns:
         dict: The daily sleep data for the user.
     """
-    return garth_client.connectapi(
+    return client.connectapi(
         f"/wellness-service/wellness/dailySleepData/{garth_client.username}",
         params={"date": datetime.date.today().isoformat(),
                 "nonSleepBufferMinutes": 60},
     )
 
 
-def stress_data(garth_client: Client) -> dict:
+def stress_data(client: GarthClient) -> dict:
     """
     Retrieves the daily stress data from the Garmin Connect API for the current date.
 
     Args:
-        garth_client (Client): The Garmin Connect API client.
+        client (GarthClient): The Garmin Connect API client.
 
     Returns:
         dict: The daily stress data for the current date for the user.
     """
-    return garth_client.connectapi(
+    return client.connectapi(
         f"/wellness-service/wellness/dailyStress/{datetime.date.today().isoformat()}")
 
 
-def lambda_handler(event, context):
+def lambda_handler(_event, _context) -> None:
     """
     Lambda function that logs into a Garmin account, retrieves sleep and stress data, and uploads it to an S3 bucket.
 
@@ -76,15 +76,15 @@ def lambda_handler(event, context):
     :return: None
     """
 
-    sleep_json_data = sleep_data(garth_client=garth_client)
-    stress_json_data = stress_data(garth_client=garth_client)
+    sleep_json_data = sleep_data(garth_client)
+    stress_json_data = stress_data(garth_client)
 
-    client.put_object(
+    s3_client.put_object(
         Bucket=garmin_s3_bucket,
         Key=f'sleep/{datetime.date.today().isoformat()}/{uuid.uuid4()}.json',
         Body=json.dumps(sleep_json_data)
     )
-    client.put_object(
+    s3_client.put_object(
         Bucket=garmin_s3_bucket,
         Key=f'stress/{datetime.date.today().isoformat()}/{uuid.uuid4()}.json',
         Body=json.dumps(stress_json_data)
